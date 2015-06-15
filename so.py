218ff32088891d5ae135a7815c0fc8f89329b2db
Andrew Brown <agbrown@ucdavis.edu>
2015-06-15 11:54:11 -0400

    Parsing is working down to subgroup level. Now need to refine formatting of special characters and check for completeness. Also, will need to refine the search routine to handle diagnostic material and family-level keys in Ch 3 and Ch 17.

--- so.py
+++ so.py
@@ -2,25 +2,71 @@
 #script, filename = argv
 import re
 
+buf=""
+
 filename="keys.xml"
+fileout="keysout.html"
 txt = open(filename)
 i=0
-mode=0
-#lines=txt.readlines()
-for l in txt:
+mode=-1
+start=-1
+end=-1
+lines=txt.readlines()
+txt.close()
+
+def print2(what):
+	global buf
+	#print what
+	buf+=''.join(what)
+	buf+='\n'
+	return
+
+for l in lines:
 	i=i+1
+	m=None
+	if mode == -1: m=re.search('<b>Key to Soil Orders</b>',l)
+	if m != None: mode=0
 	if mode==0:
-		m1=re.search('font=\"3\">([A-Z]{1,4})\.\s\s?(.*)<',l)
+		m1=re.search('\">([A-Z]{0,4})\.\s\s?.*<',l)
 		if m1 != None:
 			mode=1 #we are inside a taxonomic clause
-			print m1.groups() 
-			print i
+			print2(m1.group(1)) 
+			start=i
+			#print i
 	if mode==1:
-		m2=re.search('font=\"8\"><b>(.*)</b>?(,\sp.\s(\d+))?<',l)
+		m2=re.search('font=\"\d+\"><b>(.{2,})</b>(?:,\sp.\s(\d+))?<',l)
 		if m2 != None:
 			mode=0
-			print m2.groups()
-			print i
-			print "--------------------------"
+			print2("<TAXON>"+m2.group(1)+"</TAXON>")
+			if m2.group(2) != None:
+				print2(m2.group(2))
+			end=i
+			#prin i
+			last=-1
+			nest=0
+			for ll in lines[(start-1):end]:
+			  	#buf+=ll
+				#buf+='\n'
+				m3=re.search('<text\stop="\d{3,}"(?:.*)">(.*)</text>',ll)
+				if m3 != None:
+					m4=re.search('.([1-9a-z]).\s\s',m3.group(1))
+					if m4 != None:
+						bullet=m4.group(1)
+						if bullet.isdigit()==True:
+							toggle=0
+						else:
+							if bullet.isupper():
+								toggle=1
+							else:
+								toggle=2	 
+						if last!=toggle: nest+=1
+						last=toggle
+						print2('\n')
+					print2((nest*'\t')+m3.group(1))
+			#print2(buf)
+			print2("--------------------------")
+out=open(fileout,'w')
+out.write(buf)
+out.close()
 
 
